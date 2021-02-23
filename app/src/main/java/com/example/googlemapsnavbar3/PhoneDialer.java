2f6bc09f3707e0df3dfac4614f0317d95aad24ce
abinash3 <RameshA@cardiff.ac.uk>
2021-02-23 12:27:43 +0000

    added mingrui's phone dialer xml and class

---
+++ app/src/main/java/com/example/googlemapsnavbar3/PhoneDialer.java
@@ -0,0 +1,52 @@
+package com.example.googlemapsnavbar3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PhoneDialer extends Activity {
+
+    //Declare control objects
+    private EditText et_phone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//Set the view to display
+        setContentView(R.layout.phone_dialer);
+
+//Find controls in view
+        et_phone = (EditText)findViewById(R.id.et_phone);
+
+    }
+
+    /**
+     * The realization of dialing
+     * @param v
+     */
+    public void callPhone(View v){
+//obtain the entered phone number
+        String phone = et_phone.getText().toString();
+
+
+        if(!TextUtils.isEmpty(phone)){
+
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+phone));
+
+
+//Execution intention
+            startActivity(intent);
+
+
+        }else{
+
+            Toast.makeText(this, "Please input mobile phone number", Toast.LENGTH_LONG).show();
+
+        }
+    }
+}
+
