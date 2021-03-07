5f879fa43b0a266c23888c6663aff314685b98db
abinash3 <RameshA@cardiff.ac.uk>
2021-03-07 21:56:04 +0000

    Google Sign In fixed(needs cleaning)

--- app/src/main/java/com/example/googlemapsnavbar3/MainActivity.java
+++ app/src/main/java/com/example/googlemapsnavbar3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.googlemapsnavbar3;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -8,12 +9,28 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import org.xml.sax.SAXException;
 
@@ -49,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
         //navigation end
     }
+
 
     public void saveLocations(View view){
         PlaceFileHandler.savePlacesToFile("safePlaces.txt",this.getApplicationContext());
