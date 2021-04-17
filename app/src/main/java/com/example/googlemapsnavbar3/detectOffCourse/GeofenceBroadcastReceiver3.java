a447929017a2fbca47d66e04f8b56bf3d241fec2
SinghM21 <manis3r3@gmail.com>
2021-04-17 02:32:09 +0100

    Created Geofence classes

---
+++ app/src/main/java/com/example/googlemapsnavbar3/detectOffCourse/GeofenceBroadcastReceiver3.java
@@ -0,0 +1,4 @@
+package com.example.googlemapsnavbar3.detectOffCourse;
+
+public class GeofenceBroadcastReceiver3 {
+}
---
+++ app/src/main/java/com/example/googlemapsnavbar3/detectOffCourse/GeofenceBuilder3.java
@@ -0,0 +1,4 @@
+package com.example.googlemapsnavbar3.detectOffCourse;
+
+public class GeofenceBuilder3 {
+}
